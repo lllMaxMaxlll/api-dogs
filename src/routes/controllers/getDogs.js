006665ea3ce9d912234b9ce4c68ae94e035e3de8
lllMaxMaxlll <max.herr.88@gmail.com>
2023-06-13 18:03:29 -0300

    Add query validate

--- src/routes/controllers/getDogs.js
+++ src/routes/controllers/getDogs.js
@@ -1,16 +1,19 @@
 const { Dog } = require("../../database");
 
 const getDogs = async (order, pageNumber, attribute, limit) => {
-	const pageSize = limit?.replace(/\D/g, "");
+	const pageSize = parseInt(limit?.replace(/\D/g, ""));
 	const sortBy = attribute || "id";
 	const sortOrder = order === "desc" ? "DESC" : "ASC";
-	const offset = (pageNumber - 1) * pageSize || 0;
+	const offset = parseInt((pageNumber - 1) * pageSize) || 0;
+
+	const validAttribute = ["id", "name", "color", "tail_length", "weight"].includes(sortBy);
+	if (!validAttribute) throw new Error("Attribute is not valid");
 
 	const dogs = await Dog.findAll({
 		attributes: ["name", "color", "tail_length", "weight"],
 		order: [[sortBy, sortOrder]],
-		offset: parseInt(offset),
-		pageSize: parseInt(pageSize) ?? undefined,
+		offset: offset,
+		pageSize: pageSize,
 	});
 
 	return dogs;
