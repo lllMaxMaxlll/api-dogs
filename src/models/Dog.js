3ed6b9d2086833b731b8cf959790f41720fcb7fe
lllMaxMaxlll <max.herr.88@gmail.com>
2023-06-13 10:55:36 -0300

    add validate to model

--- src/models/Dog.js
+++ src/models/Dog.js
@@ -1,28 +1,41 @@
 const { DataTypes } = require("sequelize");
 
 module.exports = (sequelize) => {
-	sequelize.define("dog", {
-		id: {
-			type: DataTypes.INTEGER,
-			autoIncrement: true,
-			primaryKey: true,
-			allowNull: false,
+	sequelize.define(
+		"dog",
+		{
+			id: {
+				type: DataTypes.INTEGER,
+				autoIncrement: true,
+				primaryKey: true,
+			},
+			name: {
+				type: DataTypes.STRING,
+				allowNull: false,
+			},
+			color: {
+				type: DataTypes.STRING,
+				allowNull: false,
+			},
+			tail_length: {
+				type: DataTypes.INTEGER,
+				allowNull: false,
+				validate: {
+					isInt: true,
+					min: 0,
+				},
+			},
+			weight: {
+				type: DataTypes.INTEGER,
+				allowNull: false,
+				validate: {
+					isInt: true,
+					min: 0,
+				},
+			},
 		},
-		name: {
-			type: DataTypes.STRING,
-			allowNull: false,
-		},
-		color: {
-			type: DataTypes.STRING,
-			allowNull: false,
-		},
-		tail_length: {
-			type: DataTypes.INTEGER,
-			allowNull: false,
-		},
-		weigth: {
-			type: DataTypes.INTEGER,
-			allowNull: false,
-		},
-	});
+		{
+			timestamps: false,
+		}
+	);
 };
