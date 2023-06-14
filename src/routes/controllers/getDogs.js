0e9292ec216edc50ee98bd85b991a43c6bdf6908
lllMaxMaxlll <max.herr.88@gmail.com>
2023-06-14 09:22:08 -0300

    add default value, add validates data

--- src/routes/controllers/getDogs.js
+++ src/routes/controllers/getDogs.js
@@ -1,19 +1,21 @@
 const { Dog } = require("../../database");
 
-const getDogs = async (order, pageNumber, attribute, limit) => {
+const getDogs = async (order, pageNumber, attribute = "id", limit) => {
 	const pageSize = parseInt(limit?.replace(/\D/g, ""));
-	const sortBy = attribute || "id";
+	const sortBy = attribute;
 	const sortOrder = order === "desc" ? "DESC" : "ASC";
-	const offset = parseInt((pageNumber - 1) * pageSize) || 0;
+	const offset = parseInt((pageNumber - 1) * pageSize);
 
+	// Validate
 	const validAttribute = ["id", "name", "color", "tail_length", "weight"].includes(sortBy);
 	if (!validAttribute) throw new Error("Attribute is not valid");
+	if (offset < 0) throw new Error("Page number must to be major of 1");
 
 	const dogs = await Dog.findAll({
 		attributes: ["name", "color", "tail_length", "weight"],
 		order: [[sortBy, sortOrder]],
-		offset: offset,
-		pageSize: pageSize,
+		offset,
+		pageSize,
 	});
 
 	return dogs;
